a9257a7e3894c4f666d8d758b85f4f78ad1cd2eb
DraconMarius <113488982+DraconMarius@users.noreply.github.com>
2022-10-08 18:49:20 -0700

    fixed search refresh, and linked from title page

--- script.js
+++ script.js
@@ -1,19 +1,30 @@
-function clickMeButtonOnClick(event) {
-    document.location.replace("./search-results.html");
-}
+//when search is clicked, links to new page and storing the
+//value entered in form to local storage
+
+$(function clickMeButton() {
+  $(".btn-primary").on("click", function (event) {
+    event.preventDefault();
+    var searchQuery = $("#exampleFormControlInput1").val();
+    var searchFormat = $("#exampleFormControlSelect1").val();
+    localStorage.setItem("searchQuery", searchQuery);
+    localStorage.setItem("searchFormat", searchFormat);
+    // console.log(document.location);
+    document.location.replace("./search.html");
+  })
+});
 
 function fetchData(searchQuery, searchFormat) {
-    var url = "https://www.loc.gov/"+searchFormat+"/?q="+searchQuery+"&fo=json";
-    //var formattedUrl = url.replace(" ", "%20");
-    console.log(url);
+  var url = "https://www.loc.gov/" + searchFormat + "/?q=" + searchQuery + "&fo=json";
+  //var formattedUrl = url.replace(" ", "%20");
+  console.log(url);
 
-    fetch(url)
+  fetch(url)
     .then(function (response) {
       console.log("response", response);
-      
+
       return response.json();
     })
     .then(function (data) {
-      console.log("data",data);
+      console.log("data", data);
     });
-}
+};
